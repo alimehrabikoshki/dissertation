e1c26fe602b047fb53823b06d95caf82eb9c3e7e
alimehrabikoshki <79400736+alimehrabikoshki@users.noreply.github.com>
2021-02-28 19:10:48 +0000

    added kubernetes directory with dockerfiles for client and server containers. also added network policy, deployments, ansible test playbooks, and terraform test suite module to integrate with overall flow

---
+++ terraform/modules/test-suite/main.tf
@@ -0,0 +1,85 @@
+locals {
+    nodes = {"master ip" = var.cluster_master_ip, "worker1 ip" = var.cluster_worker1_ip, "worker2 ip" = var.cluster_worker2_ip}
+    workers = {"worker1 ip" = var.cluster_worker1_ip, "worker2 ip" = var.cluster_worker2_ip}
+
+}
+
+module "pod2pod-test" {
+    source = "../run-playbook"
+    for_each = local.nodes
+    cluster_master_ip = var.cluster_master_ip
+    cluster_worker1_ip = var.cluster_worker1_ip
+    cluster_worker2_ip = var.cluster_worker2_ip
+    k8s_ssh_private_key = var.k8s_ssh_private_key
+  #  playbook = "../../../ansible/modules/k8s_nodes/all/initialise_k8s_node.yaml"
+    playbook = "/mnt/c/Users/Ali/PycharmProjects/dissertation/ansible/modules/k8s_nodes/all/initialise_k8s_node.yaml"
+    target_host = each.value
+    cluster_master_internal_ip = var.cluster_master_internal_ip
+    cluster_worker1_internal_ip = var.cluster_worker1_internal_ip
+    cluster_worker2_internal_ip = var.cluster_worker2_internal_ip
+    cluster = var.cluster
+}
+
+module "cluster2cluster-test" {
+    depends_on = [module.pod2pod-test]
+    source = "../run-playbook"
+#add variable for CNI for additional master/worker playbooks
+    cluster_master_ip = var.cluster_master_ip
+    cluster_worker1_ip = var.cluster_worker1_ip
+    cluster_worker2_ip = var.cluster_worker2_ip
+    cluster_master_internal_ip = var.cluster_master_internal_ip
+    cluster_worker1_internal_ip = var.cluster_worker1_internal_ip
+    cluster_worker2_internal_ip = var.cluster_worker2_internal_ip
+    k8s_ssh_private_key = var.k8s_ssh_private_key
+    #playbook = "../../../ansible/modules/k8s_nodes/masters/initialise_k8s_master.yaml"
+    playbook = "/mnt/c/Users/Ali/PycharmProjects/dissertation/ansible/modules/k8s_nodes/masters/initialise_k8s_master.yaml"
+    cluster = var.cluster
+
+    target_host = var.cluster_master_ip
+}
+
+#add variable for CNI for additional master/worker playbooks
+module "intrapod-test" {
+    depends_on = [module.configure-master]
+    source = "../run-playbook"
+    cluster_master_ip = var.cluster_master_ip
+    cluster_worker1_ip = var.cluster_worker1_ip
+    cluster_worker2_ip = var.cluster_worker2_ip
+    cluster_master_internal_ip = var.cluster_master_internal_ip
+    cluster_worker1_internal_ip = var.cluster_worker1_internal_ip
+    cluster_worker2_internal_ip = var.cluster_worker2_internal_ip
+    k8s_ssh_private_key = var.k8s_ssh_private_key
+    playbook = var.cni_playbook_path
+    target_host = var.cluster_master_ip
+    cluster = var.cluster
+
+}
+module "configure-workers" {
+    depends_on = [module.configure-cni]
+    source = "../run-playbook"
+    for_each = local.workers
+    cluster_master_ip = var.cluster_master_ip
+    cluster_worker1_ip = var.cluster_worker1_ip
+    cluster_worker2_ip = var.cluster_worker2_ip
+    cluster_master_internal_ip = var.cluster_master_internal_ip
+    cluster_worker1_internal_ip = var.cluster_worker1_internal_ip
+    cluster_worker2_internal_ip = var.cluster_worker2_internal_ip
+    k8s_ssh_private_key = var.k8s_ssh_private_key
+  #  playbook = "../../../ansible/modules/k8s_nodes/workers/initialise_k8s_worker.yaml"
+    playbook = "/mnt/c/Users/Ali/PycharmProjects/dissertation/ansible/modules/k8s_nodes/workers/initialise_k8s_worker.yaml"
+    target_host = each.value
+    cluster = var.cluster
+
+}
+/*
+
+module "configure-worker2" {
+    source = "../run-playbook"
+#add variable for CNI for additional master/worker playbooks
+    cluster_master_ip = var.cluster_master_ip
+    cluster_worker1_ip = var.cluster_worker1_ip
+    cluster_worker2_ip = var.cluster_worker2_ip
+    k8s_ssh_private_key = var.k8s_ssh_private_key
+    playbook = "../initialise_k8s_worker.yaml"
+    target_host = var.cluster_worker2_ip
+}*/
---
+++ terraform/modules/test-suite/variables.tf
@@ -0,0 +1,55 @@
+
+/*
+Each playbook run has:
+playbook
+Master IP
+Worker IPs
+private key
+target host
+*/
+
+
+variable "cluster_master_ip" {
+  type        = string
+  description = "The external IP of the current cluster master"
+}
+
+variable "cluster_worker1_ip" {
+  type        = string
+  description = "The external IP of worker 1"
+}
+
+variable "cluster_worker2_ip" {
+  type = string
+  description = "The external IP of worker 2"
+}
+
+variable "cluster_master_internal_ip" {
+  type        = string
+  description = "The internal IP of the current cluster master"
+}
+
+variable "cluster_worker1_internal_ip" {
+  type        = string
+  description = "The internal IP of worker 1"
+}
+
+variable "cluster_worker2_internal_ip" {
+  type        = string
+  description = "The internal IP of worker 2"
+}
+
+variable "cluster" {
+  type = string
+  description = "cluster number"
+}
+variable "k8s_ssh_private_key" {
+  type        = string
+  description = "k8s user private key"
+}
+
+variable "cni_playbook_path" {
+  type        = string
+  description = "relative path to CNI playbook(s) (FROM CONFIGURE-CLUSTER DIRECTORY)"
+}
+
