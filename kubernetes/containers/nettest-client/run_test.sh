300ccaf444021a1248516fbac7a8d18827459f4d
KRAFT-Foods <79400736+alimehrabikoshki@users.noreply.github.com>
2021-03-09 05:22:01 +0000

    fixed test script missing server ip on ab tests need to: Finalize testing bash script (almost done - need to perhaps add CPU monitoring?) Define terraform module to run tests against each test case (done) Add support for all other CNIs  -TODO and finally analyse and chart the data -TODO

--- kubernetes/containers/nettest-client/run_test.sh
+++ kubernetes/containers/nettest-client/run_test.sh
@@ -7,7 +7,7 @@ iperf3 --client ${SERVER_IP} -t 60 --set-mss 1460 -i 1 -f m -J --logfile ${CNI}-
 iperf3 --client ${SERVER_IP} -t 60 --set-mss 1460 -i 1 -f m -J -P 8 --logfile ${CNI}-${TESTCASE}-iperf_tcp_8stream.log
 #udp test (change max bandwidth to 32gbps once all is done)
 iperf3 --client ${SERVER_IP} -t 60 --set-mss 32 -u -b 1600000000 -i 1 -f m -J --logfile ${CNI}-${TESTCASE}-iperf_udp.log
-#Run ApacheBench against the nginx service installed on the nettest server
-ab -n 10000 -c 10 -e ${CNI}-${TESTCASE}-apachebench_10stream.log
-#Run ApacheBench against the nginx service installed on the nettest server
-ab -n 50000 -c 50 -e ${CNI}-${TESTCASE}-apachebench_50stream.log
+#Run ApacheBench against the nginx service installed on the nettest server with 10 streams
+ab -n 10000 -c 10 -e ${CNI}-${TESTCASE}-apachebench_10stream.log ${SERVER_IP}
+#Run ApacheBench against the nginx service installed on the nettest server with 50 streams
+ab -n 50000 -c 50 -e ${CNI}-${TESTCASE}-apachebench_50stream.log ${SERVER_IP}
