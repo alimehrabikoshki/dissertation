575539743dd89615ad8b4b88cd241273e11c8d18
alimehrabikoshki <79400736+alimehrabikoshki@users.noreply.github.com>
2021-03-09 09:03:02 +0000

    major fixes, now runs all the way until the bash script at which point it fails due to a mistake. have corrected it now, trying again need to: Finalize testing bash script (almost done - need to perhaps add CPU monitoring?) Define terraform module to run tests against each test case (done) Add support for all other CNIs  -TODO and finally analyse and chart the data -TODO

--- terraform/modules/test-suite/main.tf
+++ terraform/modules/test-suite/main.tf
@@ -1,11 +1,4 @@
-/*locals {
-    nodes = {"master ip" = var.cluster_master_ip, "worker1 ip" = var.cluster_worker1_ip, "worker2 ip" = var.cluster_worker2_ip}
-    workers = {"worker1 ip" = var.cluster_worker1_ip, "worker2 ip" = var.cluster_worker2_ip}
-
-Target cluster1 master and provision server
-Target the 3 different cluster masters and provision clients
-}*/
-
+/*
 resource "null_resource" "save_intrazone_vars"{
   provisioner "local-exec" {
     command = <<EOT
@@ -15,6 +8,34 @@ cat <<EOF > /mnt/c/Users/Ali/PycharmProjects/dissertation/ansible/vars_files/tes
   "TESTCASE": "intrazone"
 }
 EOF
+    EOT
+  }
+}
+*/
+
+module "deploy-nettest-server" {
+    source = "../run-playbook"
+    cluster_master_ip = var.cluster_master_ip
+    cluster_worker1_ip = var.cluster_worker1_ip
+    cluster_worker2_ip = var.cluster_worker2_ip
+    k8s_ssh_private_key = var.k8s_ssh_private_key
+    playbook = "/mnt/c/Users/Ali/PycharmProjects/dissertation/ansible/modules/k8s_nodes/masters/deploy_nettest_server.yaml"
+    target_host = var.cluster1_master_public_ip
+    cluster_master_internal_ip = var.cluster_master_internal_ip
+    cluster_worker1_internal_ip = var.cluster_worker1_internal_ip
+    cluster_worker2_internal_ip = var.cluster_worker2_internal_ip
+    cluster = var.cluster
+}
+
+resource "null_resource" "save_intrazone_vars"{
+  depends_on = [module.deploy-nettest-server]
+  provisioner "local-exec" {
+    command = <<EOT
+echo "
+{
+  "CNI": "${var.cni}",
+  "TESTCASE": "intrazone"
+} " > /mnt/c/Users/Ali/PycharmProjects/dissertation/ansible/vars_files/test_vars.json
     EOT
   }
 }
@@ -34,6 +55,7 @@ module "intrazone-test" {
 }
 
 resource "null_resource" "save_interzone_vars"{
+  depends_on = [module.intrazone-test]
   provisioner "local-exec" {
     command = <<EOT
 cat <<EOF > /mnt/c/Users/Ali/PycharmProjects/dissertation/ansible/vars_files/test_vars.json
@@ -46,7 +68,7 @@ EOF
   }
 }
 module "interzone-test" {
-    depends_on = [null_resource.save_interzone_vars]
+    depends_on = [null_resource.save_interzone_vars, module.intrazone-test]
     source = "../run-playbook"
     cluster_master_ip = var.cluster_master_ip
     cluster_worker1_ip = var.cluster_worker1_ip
@@ -60,6 +82,7 @@ module "interzone-test" {
     cluster = var.cluster
 }
 resource "null_resource" "save_interregion_vars"{
+  depends_on = [module.interzone-test]
   provisioner "local-exec" {
     command = <<EOT
 cat <<EOF > /mnt/c/Users/Ali/PycharmProjects/dissertation/ansible/vars_files/test_vars.json
@@ -73,7 +96,7 @@ EOF
 }
 
 module "interregion-test" {
-    depends_on = [null_resource.save_interregion_vars]
+    depends_on = [null_resource.save_interregion_vars, module.interzone-test]
     source = "../run-playbook"
     cluster_master_ip = var.cluster_master_ip
     cluster_worker1_ip = var.cluster_worker1_ip
