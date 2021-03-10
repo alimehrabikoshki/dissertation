4faa0ab189b012710694348711318ed2172d5bc6
alimehrabikoshki <79400736+alimehrabikoshki@users.noreply.github.com>
2021-03-10 13:03:38 +0000

    Commit includes extra vars files and results from a mock test run need to: Finalize testing bash script (need to manually manipulate text outputs to automate chart creation, otherwise its a ton of work ) Define terraform module to run tests against each test case (done) Add support for all other CNIs  -TODO and finally analyse and chart the data -TODO

--- kubernetes/containers/nettest-client/run_test.sh
+++ kubernetes/containers/nettest-client/run_test.sh
@@ -5,7 +5,7 @@
 iperf3 --client ${SERVER_IP} -p 30000 -t 20 --set-mss 1460 -i 1 -f m  --logfile ${CNI}-${TESTCASE}-iperf_tcp_1stream.log
 #tcp 8 stream test
 iperf3 --client ${SERVER_IP} -p 30000 -t 20 --set-mss 1460 -i 1 -f m  -P 8 --logfile ${CNI}-${TESTCASE}-iperf_tcp_8stream.log
-#udp test (TODO:change max bandwidth to 32gbps once all is done)
+#udp test
 iperf3 --client ${SERVER_IP} -p 30000 -t 20 --set-mss 32 -u -b 32000000000 -i 1 -f m  --logfile ${CNI}-${TESTCASE}-iperf_udp.log
 #Run ApacheBench against the nginx service installed on the nettest server with 10 streams
 ab -n 10000 -c 10 -e ${CNI}-${TESTCASE}-apachebench_10stream.log http://${SERVER_IP}:30080/
