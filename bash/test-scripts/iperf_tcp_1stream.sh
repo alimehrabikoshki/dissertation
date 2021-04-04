90aa8b76b11bdd457f1555f962fc8f9d618408b5
alimehrabikoshki <--global>
2021-04-04 22:46:08 +0100

    all plotting is done, only incremental improvements and reruns to do from here -changed calico_vxlan to use eBPF as the dataplane need to: Finalize testing bash script (done  ) Define terraform module to run tests against each test case (done) Add support for all other CNIs -  done finalize result parsing scripts - done and finally analyse and chart the data - DONE

--- bash/test-scripts/iperf_tcp_1stream.sh
+++ bash/test-scripts/iperf_tcp_1stream.sh
@@ -1,1 +1,5 @@
 iperf3 --client ${SERVER_IP} -p 30000 -t 60 --set-mss 1460 -i 1 -f m -C cubic  > ${RAW_RESULT_PATH}${CNI}-${TESTCASE}-iperf_tcp_1stream.log
+
+###
+# Time        UID       PID    %usr %system  %guest   %wait    %CPU   CPU  minflt/s  majflt/s     VSZ     RSS   %MEM  Command
+#16:33:36     1000     11454    0.00    0.00    0.00    0.00    0.00     7      0.00      0.00   10412    1232   0.00  iperf3
